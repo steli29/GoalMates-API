825fc2d6afab79e928a6214f85d122ec6d7c1914
chavdar00 <chavdargoranov00@gmail.com>
2023-04-08 12:35:50 +0300

    Project creation

---
+++ src/main/java/com/example/goalmates/GoalmatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.goalmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalmatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalmatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goalmates/GoalmatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goalmates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalmatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
