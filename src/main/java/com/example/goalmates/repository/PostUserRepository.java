e65c01d2b64e6d9de89f142161f75bea6f10f595
chavdar00 <chavdargoranov00@gmail.com>
2023-08-03 23:18:24 +0300

    Add missing repository notification

--- src/main/java/com/example/goalmates/repository/PostUserRepository.java
+++ src/main/java/com/example/goalmates/repository/PostUserRepository.java
@@ -3,6 +3,8 @@ package com.example.goalmates.repository;
 import com.example.goalmates.models.PostUsers;
 import com.example.goalmates.models.PostUsersCompKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostUserRepository  extends JpaRepository<PostUsers, PostUsersCompKey> {
 }
